379cac9ab943e500b55c2c7e569987b64243ca7e
yandimant1993 <yan1993dimant@gmail.com>
2025-08-11 22:39:09 +0200

    Toy preview & Toy list

--- src/cmp/ToyPreview.jsx
+++ src/cmp/ToyPreview.jsx
@@ -1,6 +1,15 @@
+import { Link } from "react-router-dom";
 
 
-
-export function ToyPreview(){
-    
+export function ToyPreview({ toy }) {
+    return (
+        <article>
+            <h4>{toy.name}</h4>
+            <h1>⛐</h1>
+            <p>Price: <span>${toy.price.toLocaleString()}</span></p>
+            <p><img src={`${toy.imgUrl}`}/></p>
+            <Link to={`/toy/edit/${toy._id}`}>Edit</Link>
+            <Link to={`/toy/${toy._id}`}>Details</Link>
+        </article>
+    )
 }
