b79bfe780380211507ca094a7a2c9efb8f6edae7
yandimant1993 <yan1993dimant@gmail.com>
2025-08-11 23:29:21 +0200

    Create toyAction

--- src/pages/ToyIndex.jsx
+++ src/pages/ToyIndex.jsx
@@ -1,5 +1,90 @@
 
 
-export function toyIndex(){
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 
+import { ToyFilter } from '../cmp/ToyFilter.jsx'
+import { ToyList } from '../cmp/ToyList.jsx'
+import { toyService } from '../services/toy.service.js'
+import { loadToys, removeToy, saveToy, setFilterBy } from '../store/actions/toy.action.js'
+import { showErrorMsg, showSuccessMsg } from "../services/event-bus.service.js";
+
+export function ToyIndex() {
+    const dispatch = useDispatch()
+    const toys = useSelector(storeState => storeState.toyModule.toys)
+    const filterBy = useSelector(storeState => storeState.toyModule.filterBy)
+    const isLoading = useSelector(storeState => storeState.toyModule.isLoading)
+
+    useEffect(() => {
+        loadToys()
+            .catch(err => {
+                showErrorMsg('Cannot load toys!')
+            })
+    }, [filterBy])
+
+    function onSetFilter(filterBy) {
+        setFilterBy(filterBy)
+    }
+
+    function onRemoveToy(toyId) {
+        removeToy(toyId)
+            .then(() => {
+                showSuccessMsg('Toy removed')
+            })
+            .catch(err => {
+                showErrorMsg('Cannot remove toy')
+            })
+    }
+
+    function onAddToy() {
+        const toyToSave = toyService.getRandomToy()
+        saveToy(toyToSave)
+            .then((savedToy) => {
+                showSuccessMsg(`Toy added (id: ${savedToy._id})`)
+            })
+            .catch(err => {
+                showErrorMsg('Cannot add toy')
+            })
+    }
+
+    function onEditToy(toy) {
+        const price = +prompt('New price?')
+        const toyToSave = { ...toy, price }
+
+        saveToy(toyToSave)
+            .then((savedToy) => {
+                showSuccessMsg(`Toy updated to price: $${savedToy.price}`)
+            })
+            .catch(err => {
+                showErrorMsg('Cannot update toy')
+            })
+    }
+
+    function addToToy(toy) {
+        console.log(`Adding ${toy.name} to Toy`)
+        dispatch({ type: ADD_TOY_TO_TOY, toy })
+        showSuccessMsg('Added to Toy')
+    }
+
+    return (
+        <div>
+            <h3>Toys App</h3>
+            <main>
+                <Link to="/toy/edit">Add Toy</Link>
+                <button className='add-btn' onClick={onAddToy}>Add Random Toy ⛐</button>
+                <ToyFilter filterBy={filterBy} onSetFilter={onSetFilter} />
+                {!isLoading
+                    ? <ToyList
+                        toys={toys}
+                        onRemoveToy={onRemoveToy}
+                        onEditToy={onEditToy}
+                        addToToy={addToToy}
+                    />
+                    : <div>Loading...</div>
+                }
+                <hr />
+            </main>
+        </div>
+    )
 }
